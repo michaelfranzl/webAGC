aec4716a1edfc0a766fbb2bd7da349025c771abb
Michael K. Franzl <public.michael@franzl.name>
2021-05-22 18:59:33 +0200

    Add demo

---
+++ demo/README.md
@@ -0,0 +1,54 @@
+# webAGC Demo
+
+This demo combines the following components:
+
+* `webAGC` (wraps a WebAssembly build of `yaAGC`)
+* `webDSKY` (an emulator of the user interface of the AGC)
+* Control elements
+  * CPU manipulation: reset, halt, run, step, clock divide
+  * 'Hot-plugging' of the "fixed memory" (the 'ROM') of the AGC. In other words, plugging different
+      programs into the computer while it is running.  (see below, "AGC programs")
+* A live view of the "erasable memory" (the 'RAM') of the AGC
+
+## AGC programs
+
+This demo allows the selection of the following AGC programs which are contained in this repository
+in binary form.  A link to the exact state of the source code and build scripts is given in the
+third column of the table.
+
+Documentation of these AGC programs can be found at https://www.ibiblio.org/apollo/
+
+<table>
+<thead>
+  <tr>
+    <th>Name</th>
+    <th>Binary</th>
+    <th>Sources</th>
+    <th>Notes</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>Luminary099</td>
+    <td><a href="agc/Luminary099.bin">agc/Luminary099.bin</a></td>
+    <td><a href="https://github.com/virtualagc/virtualagc/tree/92a74ef1c88aede2e4ba0a86c6585c9ea911226d/Luminary099">Source</a></td>
+    <td>Built by running <code>make</code> in the root directory.</td>
+  </tr>
+  <tr>
+    <td>Validation</td>
+    <td><a href="agc/Validation.bin">agc/Validation.bin</a></td>
+    <td><a href="https://github.com/virtualagc/virtualagc/tree/92a74ef1c88aede2e4ba0a86c6585c9ea911226d/Validation">Source</a></td>
+    <td>Built by running <code>make</code> in the root directory.</td>
+  </tr>
+</tbody>
+</table>
+
+
+## How to run
+
+In this directory
+
+1. run `npm i -g jspm@2.0.0-beta.7`
+2. run `jspm install`
+3. serve the parent directory via HTTP.
+4. access `demo/index.html` using a browser supporting [import maps](https://caniuse.com/?search=importmap) (e.g. based on Chromium 89 or newer). For example, visit: http://localhost:8000/demo/
---
+++ demo/index.js
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: Copyright 2020 Michael Karl Franzl <public.michael@franzl.name>
+
+import 'webdsky';
+import WebAGC from '../src/webAGC.js';
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+  return [...new Uint8Array(buffer)].map(x => x.toString(16).padStart(2, '0')).join('');
+}
+
+window.addEventListener('load', async (_) => {
+  console.log('onload');
+  main();
+});
+
+async function main() {
+  // Create a DSKY instance.
+  const dsky = createDsky();
+
+  // Create a yaAGC instance.
+  const agc = new WebAGC({
+    // Wire the AGC lamp and digit outputs to the DSKY.
+    dskyOut: ({ input1, input2 }) => {
+      if (input1 !== undefined)
+        dsky.setAttribute('data-input1', input1.toString(2).padStart(15, '0'));
+      if (input2 !== undefined)
+        dsky.setAttribute('data-input2', input2.toString(2).padStart(15, '0'));
+    },
+  });
+
+  // Wire the DSKY key presses to the AGC (the PRO key is wired separately).
+  dsky.addEventListener('keypress', ({ detail }) => agc.keyPress(detail));
+  dsky.addEventListener('proceed', ({ detail }) => agc.proceedKeyPress(detail));
+
+  const agcReady = agc.ready().then(() => console.log(`AGC version ${agc.version()} is ready`));
+  await Promise.all([agcReady]);
+
+  const erasable = agc.getErasable();
+  const wordEls = [];
+
+  // Start the virtual computer clock.
+  agc.oscillate();
+  viewErasableMemory();
+
+  async function runProgram(binary) {
+    const checksum = await crypto.subtle.digest('SHA-1', binary);
+    console.log('Loading ROM with SHA-1 checksum', buf2hex(checksum));
+    agc.loadRom(binary);
+    agc.reset();
+  }
+
+  async function loadProgram(url) {
+    const response = await fetch(url);
+    const binary = await response.arrayBuffer();
+    runProgram(binary);
+  }
+
+  function viewErasableMemory() {
+    const domEl = document.getElementById('erasable');
+    for (let bankIdx = 0; bankIdx < 8; bankIdx++) {
+      const bankEl = document.createElement('div');
+      bankEl.classList.add('bank');
+      const headingEl = document.createElement('p');
+      headingEl.innerHTML = `Bank ${parseInt(bankIdx, 8)}`;
+      bankEl.appendChild(headingEl);
+      domEl.appendChild(bankEl);
+      for (let rowIdx = 0; rowIdx < 32; rowIdx++) {
+        const rowEl = document.createElement('div');
+        bankEl.appendChild(rowEl);
+        for (let wordIdx = 0; wordIdx < 8; wordIdx++) {
+          const wordEl = document.createElement('span');
+          wordEl.classList.add('word');
+          wordEl.wordValue = 0;
+          wordEl.innerHTML = '....';
+          rowEl.appendChild(wordEl);
+          wordEls.push(wordEl);
+        }
+      }
+    }
+    updateErasableMemory();
+  }
+
+  function updateErasableMemory() {
+    for (let idx = 0; idx < erasable.length; idx++) {
+      const wordEl = wordEls[idx];
+      const val = erasable[idx];
+      if (wordEl.wordValue === val) continue; // Skip unnecessary expensive DOM updates.
+
+      wordEl.wordValue = val;
+      wordEl.innerHTML = `0000${val.toString(16)}`.slice(-4); // hexadecimal output of a 16 bit word
+    }
+    window.requestAnimationFrame(updateErasableMemory);
+  }
+
+  const clockDivisorElement = document.getElementById('clock_divisor');
+
+  function getClockDivisor() {
+    switch (clockDivisorElement.value) {
+      case 'inf':
+        return Infinity;
+      default:
+        return parseInt(clockDivisorElement.value, 10);
+    }
+  }
+  clockDivisorElement.addEventListener('change', (event) => {
+    const divisor = event.target.value === 'inf' ? Infinity : parseInt(event.target.value, 10);
+    agc.oscillate(divisor);
+    const cycleTimeUs = 11.72;
+    document.getElementById('clock_speed')
+      .innerHTML = `${(1 / cycleTimeUs * 1000000 / divisor).toFixed(1)} Hz`;
+  });
+
+  document.getElementById('programs').addEventListener('change', (event) => {
+    const programName = event.target.value;
+    loadProgram(`agc/${programName}`);
+  });
+
+  document.getElementById('button_reset')
+    .addEventListener('click', () => {
+      // TODO: Clear DSKY digits and lights
+      agc.reset();
+    });
+
+  document.getElementById('button_halt')
+    .addEventListener('click', () => agc.oscillate(Infinity));
+
+  document.getElementById('button_run')
+    .addEventListener('click', () => agc.oscillate(getClockDivisor()));
+
+  document.getElementById('button_step')
+    .addEventListener('click', () => agc.stepCpu(1)());
+
+  document.getElementById('clock_divisor').value = 1;
+  document.getElementById('clock_divisor').dispatchEvent(new Event('change'));
+}
+
+
+function createDsky() {
+  const dsky = document.createElement('dsky-interface');
+  document.getElementById('dsky').appendChild(dsky);
+
+  const input = document.getElementById('dsky_key_input');
+  input.addEventListener('keydown', (event) => {
+    let key;
+    if (event.key === '+') key = 'p';
+    else if (event.key === '-') key = 'm';
+    else if (event.key === 'Enter') key = 'e';
+    else key = event.key.toLowerCase();
+    dsky.setAttribute('data-keypress', key);
+  });
+  input.addEventListener('keyup', () => dsky.setAttribute('data-keypress', ''));
+  return dsky;
+}
